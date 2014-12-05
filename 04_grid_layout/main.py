0168c12033111af677738058c2862d9d96b9a2b5
KJxfceChromebook <maxpowertransfer@gmail.com>
2014-12-04 22:27:12 -0500

    Remove separators, add README

--- 04_grid_layout/main.py
+++ 04_grid_layout/main.py
@@ -10,7 +10,7 @@ def quitapp():
 #################################################
 mainwindow = Tk()
 
-topframe = Frame(mainwindow)
+topframe = Frame(mainwindow, padx=10, pady=10)
 topframe.pack(side=LEFT)
 
 label_name = Label(topframe, text='Name: ')
@@ -23,22 +23,24 @@ entry_name.grid(row=0, column=1)
 entry_email = Entry(topframe)
 entry_email.grid(row=1, column=1)
 
-separator1 = Frame(topframe, height=2, bd=1, relief=SUNKEN)
-separator1.grid(row=3, columnspan=2, padx=5, pady=5)
+#separator1 = Frame(topframe, height=2, bd=1, relief=SUNKEN)
+#separator1.grid(row=3, columnspan=2, padx=5, pady=5)
 
 text_output = Text(topframe)
 text_output.config(height=1, width=30)
 text_output.grid(row=4, column=0, columnspan=2)
 
-separator2 = Frame(mainwindow, height=2, bd=1, relief=SUNKEN)
-separator2.pack()#fill=X, padx=5, pady=5)
+#separator2 = Frame(mainwindow, height=2, bd=1, relief=SUNKEN)
+#separator2.pack() #fill=X, padx=5, pady=5)
+#separator1 = Frame(topframe, height=2, bd=1, relief=SUNKEN)
+#separator1.grid(row=5, columnspan=2, padx=5, pady=5)
 
 #bottomframe = Frame(mainwindow)
 #bottomframe.pack(side=RIGHT)
 button_submit = Button(topframe, text="Enter")
-button_submit.grid(row=5, column=0)
+button_submit.grid(row=6, column=0)
 button_quit = Button(topframe, text="Quit", command = quitapp)
-button_quit.grid(row=5, column=1, sticky=E)
+button_quit.grid(row=6, column=1, sticky=E)
 
 mainwindow.title("My App")
 mainwindow.mainloop()
